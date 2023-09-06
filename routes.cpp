269ab3076eb5eeb931b487a94109ccd8bb387949
Madhan <madhansundaram19@gmail.com>
2023-09-06 14:05:01 +0530

    bg

--- routes.cpp
+++ routes.cpp
@@ -4,7 +4,7 @@ crow::response greet()
 {
     return crow::response{""};
 }
-crow::response add(const crow::request &req)
+crow::response afdd(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
     return crow::response{""};
